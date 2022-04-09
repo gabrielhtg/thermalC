bcf34f2783c5e2b7c9928c1ca293fac2c7fe2abd
gabrielhtg <gabrielhutagalung970@gmail.com>
2022-04-09 09:12:21 +0700

    coding improvement

--- thermal-v1.c
+++ thermal-v1.c
@@ -15,142 +15,11 @@ int main () {
     int tujuan;         // merupakan angka yang digunakan untuk menentukan satuan suhu tujuan konversi
     char start[20];     // merupakan pendefenisian nama bantuan untuk output berupa string untuk satuan suhu awal
     char akhir[20];     // merupakan pendefenisian nama bantuan untuk output berupa string untuk satuan suhu tujuan
-
-    printf("Satuan suhu apa yang sekarang kamu gunakan?\n");
-    printf("    1. Celcius\n");
-    printf("    2. Fahrenheit\n");
-    printf("    3. Reamur\n");
-    printf("    4. Kelvin\n");
-    printf("Masukkan pilihan kamu sesuai dgn angkanya: ");
-    scanf("%d", &awal);
-    system("clear");
-
-    if (awal == 1) {
-        strcpy(start, "derajat Celcius");
-    }
-
-    else if (awal == 2) {
-        strcpy(start, "derajat Fahrenheit");
-    }
-
-    else if (awal == 3) {
-        strcpy(start, "derajat Reamur");
-    }
-
-    else if (awal == 4) {
-        strcpy(start, "Kelvin");
-    }
-
-    printf("\nBaik. Berarti kamu sekarang dalam satuan suhu %s.\n", start);
-    printf("Kamu konversi ke satuan apa?\n\n");
-    printf("    1. Celcius\n");
-    printf("    2. Fahrenheit\n");
-    printf("    3. Reamur\n");
-    printf("    4. Kelvin\n");
-    printf("Masukkan pilihan kamu sesuai dgn angkanya: ");
-    scanf("%d", &tujuan);
-    system("clear");
-
-    if (tujuan == 1) {
-        strcpy(akhir, "derajat Celcius");
-    }
-
-    else if (tujuan == 2) {
-        strcpy(akhir, "derajat Fahrenheit");
-    }
-
-    else if (tujuan == 3) {
-        strcpy(akhir, "derajat Reamur");
-    }
-
-    else if (tujuan == 4) {
-        strcpy(akhir, "Kelvin");
-    }
-
     double thermal;
-
-    printf("Baik. Berarti kamu akan mengonversi dari %s ke %s\n", start, akhir);
-    printf("Sekarang, masukkan nilai suhu yang akan kamu konversi: ");
-    scanf("%lf", & thermal);
-    system("clear");
-
     double hasil;
-
-    if (awal == tujuan) {
-        printf("Hasil koversinya adalah %0.2lf %s\n", thermal, akhir);
-    }
-
-    else if (awal == 1 && tujuan == 2) {
-        hasil = ((thermal * 9) / 5) + 32;
-        printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
-    }
-
-    else if (awal == 1 && tujuan == 3) {
-        hasil = (thermal * 4) / 5;
-        printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
-    }
-
-    else if (awal == 1 && tujuan == 4) {
-        hasil = thermal + 273.15;
-        printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
-    }
-
-    else if (awal == 2 && tujuan == 1) {
-        hasil = ((thermal - 32) * 5) / 9;
-        printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
-    }
-
-    else if (awal == 2 && tujuan == 3) {
-        hasil = ((thermal - 32) * 4) / 9;
-        printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
-    }
-
-    else if (awal == 2 && tujuan == 4) {
-        hasil = (((thermal - 32) * 5) / 9) + 273.15;
-        printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
-    }
-
-    else if (awal == 3 && tujuan == 1) {
-        hasil = (thermal * 5) / 4;
-        printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
-    }
-
-    else if (awal == 3 && tujuan == 2) {
-        hasil = ((thermal * 9) / 4) + 32;
-        printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
-    }
-
-    else if (awal == 3 && tujuan == 4) {
-        hasil = ((thermal * 5) / 9) + 273.15;
-        printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
-    }
-
-    else if (awal == 4 && tujuan == 1) {
-        hasil = (thermal - 273.15);
-        printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
-    }
-
-    else if (awal == 4 && tujuan == 2) {
-        hasil = (((thermal - 273.15) * 9) / 5) + 32;
-        printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
-    }
-
-    else if (awal == 4 && tujuan == 3) {
-        hasil = ((thermal - 273.15) * 4) /5;
-        printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
-    }
-
     char yesno[1];
     int iya;
     int engga;
-
-    printf("Apakah masih mau melakukan konversi?\n");
-    printf("Yes (y) or no (n): \n");
-    scanf("%s", &yesno);
-    system("clear");
-
-    iya =  strcmp("y", yesno);
-    engga = strcmp("n", yesno);
 
     while (iya == 0) {
         printf("Satuan suhu apa yang sekarang kamu gunakan?\n");
@@ -210,67 +79,67 @@ int main () {
         system("clear");
 
         if (awal == tujuan) {
-            printf("Hasil koversinya adalah %0.2lf %s\n", thermal, akhir);
+            printf("Hasil koversinya adalah %0.2lf %s\n\n", thermal, akhir);
         }
 
         else if (awal == 1 && tujuan == 2) {
             hasil = ((thermal * 9) / 5) + 32;
-            printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
+            printf("Hasil konversinya adalah %0.2lf %s\n\n", hasil, akhir);
         }
 
         else if (awal == 1 && tujuan == 3) {
             hasil = (thermal * 4) / 5;
-            printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
+            printf("Hasil konversinya adalah %0.2lf %s\n\n", hasil, akhir);
         }
 
         else if (awal == 1 && tujuan == 4) {
             hasil = thermal + 273.15;
-            printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
+            printf("Hasil konversinya adalah %0.2lf %s\n\n", hasil, akhir);
         }
 
         else if (awal == 2 && tujuan == 1) {
             hasil = ((thermal - 32) * 5) / 9;
-            printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
+            printf("Hasil konversinya adalah %0.2lf %s\n\n", hasil, akhir);
         }
 
         else if (awal == 2 && tujuan == 3) {
             hasil = ((thermal - 32) * 4) / 9;
-            printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
+            printf("Hasil konversinya adalah %0.2lf %s\n\n", hasil, akhir);
         }
 
         else if (awal == 2 && tujuan == 4) {
             hasil = (((thermal - 32) * 5) / 9) + 273.15;
-            printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
+            printf("Hasil konversinya adalah %0.2lf %s\n\n", hasil, akhir);
         }
 
         else if (awal == 3 && tujuan == 1) {
             hasil = (thermal * 5) / 4;
-            printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
+            printf("Hasil konversinya adalah %0.2lf %s\n\n", hasil, akhir);
         }
 
         else if (awal == 3 && tujuan == 2) {
             hasil = ((thermal * 9) / 4) + 32;
-            printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
+            printf("Hasil konversinya adalah %0.2lf %s\n\n", hasil, akhir);
         }
 
         else if (awal == 3 && tujuan == 4) {
             hasil = ((thermal * 5) / 9) + 273.15;
-            printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
+            printf("Hasil konversinya adalah %0.2lf %s\n\n", hasil, akhir);
         }
 
         else if (awal == 4 && tujuan == 1) {
             hasil = (thermal - 273.15);
-            printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
+            printf("Hasil konversinya adalah %0.2lf %s\n\n", hasil, akhir);
         }
 
         else if (awal == 4 && tujuan == 2) {
             hasil = (((thermal - 273.15) * 9) / 5) + 32;
-            printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
+            printf("Hasil konversinya adalah %0.2lf %s\n\n", hasil, akhir);
         }
 
         else if (awal == 4 && tujuan == 3) {
             hasil = ((thermal - 273.15) * 4) /5;
-            printf("Hasil konversinya adalah %0.2lf %s\n", hasil, akhir);
+            printf("Hasil konversinya adalah %0.2lf %s\n\n", hasil, akhir);
         }
 
         printf("Apakah masih mau melakukan konversi?\n");
--- thermal-v2.c
+++ thermal-v2.c
@@ -17,32 +17,6 @@ int main () {
     int awal;
     char start[20];
 
-    printf("Satuan suhu apa yang sekarang kamu gunakan?\n");
-    printf("    1. Celcius\n");
-    printf("    2. Fahrenheit\n");
-    printf("    3. Reamur\n");
-    printf("    4. Kelvin\n");
-    printf("Masukkan pilihan kamu sesuai dgn angkanya: ");
-    scanf("%d", &awal);
-    system("clear");
-    
-
-    if (awal == 1) {
-        strcpy(start, "derajat Celcius");
-    }
-
-    else if (awal == 2) {
-        strcpy(start, "derajat Fahrenheit");
-    }
-
-    else if (awal == 3) {
-        strcpy(start, "derajat Reamur");
-    }
-
-    else if (awal == 4) {
-        strcpy(start, "Kelvin");
-    }
-
     double celc;
     double fah;
     double ream;
@@ -54,68 +28,9 @@ int main () {
     char reamur[] = "derajat Reamur";
     char kelvin[] = "Kelvin";
 
-    printf("Baik satuan awal kamu adalah %s\n", start);
-    printf("Sekarang, masukkan nilai suhu yang akan kamu konversi: ");
-    scanf("%lf", & thermal);
-    system("clear");
-    
-    
-    if (awal == 1) {
-        fah = ((thermal * 9) / 5) + 32;
-        ream = (thermal * 4) / 5;
-        kelv = thermal + 273.15;
-        printf("Satuan awal kamu adalah: %0.2lf %s\n\n", thermal, celcius);
-        printf("Hasil konversinya ke Fahrenheit     : %0.2lf %s\n", fah, fahrenheit);
-        printf("Hasil konversinya ke Reamur         : %0.2lf %s\n", ream, reamur);
-        printf("Hasil konversinya ke Kelvin         : %0.2lf %s\n\n", kelv, kelvin);
-        
-    }
-
-    else if (awal == 2) {
-        celc = ((thermal - 32) * 5) / 9;
-        ream = ((thermal - 32) * 4) / 9;
-        kelv = (((thermal - 32) * 5) / 9) + 273.15;
-        printf("Satuan awal kamu adalah: %0.2lf %s\n\n", thermal, fahrenheit);
-        printf("Hasil konversinya ke Celcius    : %0.2lf %s\n", celc, celcius);
-        printf("Hasil konversinya ke Reamur     : %0.2lf %s\n", ream, reamur);
-        printf("Hasil konversinya ke Kelvin     : %0.2lf %s\n\n", kelv, kelvin);
-        
-    }
-    
-    else if (awal == 3) {
-        celc = (thermal * 5) / 4;
-        fah = ((thermal * 9) / 4) + 32;
-        kelv = ((thermal * 5) / 9) + 273.15;
-        printf("Satuan awal kamu adalah: %0.2lf %s\n\n", thermal, reamur);
-        printf("Hasil konversinya ke Celcius    : %0.2lf %s\n", celc, celcius);
-        printf("Hasil konversinya ke Fahrenheit : %0.2lf %s\n", fah, fahrenheit);
-        printf("Hasil konversinya ke Kelvin     : %0.2lf %s\n\n", kelv, kelvin);
-        
-    }
-
-    else if (awal == 4) {
-        celc = (thermal - 273.15);
-        fah = (((thermal - 273.15) * 9) / 5) + 32;
-        ream = ((thermal - 273.15) * 4) /5;
-        printf("Satuan awal kamu adalah: %0.2lf %s\n\n", thermal, kelvin);
-        printf("Hasil konversinya ke Celcius    : %0.2lf %s\n", celc, celcius);
-        printf("Hasil konversinya ke Fahrenheit : %0.2lf %s\n", fah, fahrenheit);
-        printf("Hasil konversinya ke Reamur     : %0.2lf %s\n\n", ream, reamur);
-        
-    }
-
     char yesno[1];
-    int iya;
+    int iya = 0;
     int engga;
-
-    printf("Apakah masih mau melakukan konversi?\n");
-    printf("Yes (y) or no (n): \n");
-    scanf("%s", &yesno);
-    system("clear");
-    
-
-    iya =  strcmp("y", yesno);
-    engga = strcmp("n", yesno);
 
     while (iya == 0) {
         printf("Satuan suhu apa yang sekarang kamu gunakan?\n");
